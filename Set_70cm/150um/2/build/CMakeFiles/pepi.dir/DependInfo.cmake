7f941ff2d99d168be6076f2d2a19bb7809e7ea71
Tserriednich <t.andrade@uniandes.edu.co>
2025-04-21 16:32:47 -0500

    More things..

--- Set_70cm/150um/2/build/CMakeFiles/pepi.dir/DependInfo.cmake
+++ Set_70cm/150um/2/build/CMakeFiles/pepi.dir/DependInfo.cmake
@@ -4,21 +4,21 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/t.andrade/F1/pepi.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/pepi.cc.o"
-  "/home/t.andrade/F1/src/PepiActionInitialization.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o"
-  "/home/t.andrade/F1/src/PepiController.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiController.cc.o"
-  "/home/t.andrade/F1/src/PepiControllerMessenger.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o"
-  "/home/t.andrade/F1/src/PepiDetectorConstruction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o"
-  "/home/t.andrade/F1/src/PepiDetectorMessenger.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o"
-  "/home/t.andrade/F1/src/PepiEventAction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiEventAction.cc.o"
-  "/home/t.andrade/F1/src/PepiPSPixiRad.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o"
-  "/home/t.andrade/F1/src/PepiPhysicsList.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o"
-  "/home/t.andrade/F1/src/PepiPhysicsListMessenger.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o"
-  "/home/t.andrade/F1/src/PepiPhysicsXrayRefraction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o"
-  "/home/t.andrade/F1/src/PepiPrimaryGeneratorAction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o"
-  "/home/t.andrade/F1/src/PepiRun.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiRun.cc.o"
-  "/home/t.andrade/F1/src/PepiRunAction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiRunAction.cc.o"
-  "/home/t.andrade/F1/src/PepiXrayRefraction.cc" "/home/t.andrade/F1/build/CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/pepi.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/pepi.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiActionInitialization.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiController.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiController.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiControllerMessenger.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiDetectorConstruction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiDetectorMessenger.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiEventAction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiEventAction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiPSPixiRad.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiPhysicsList.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiPhysicsListMessenger.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiPhysicsXrayRefraction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiPrimaryGeneratorAction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiRun.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiRun.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiRunAction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiRunAction.cc.o"
+  "/home/t.andrade/Set_70cm/150um/2/src/PepiXrayRefraction.cc" "/home/t.andrade/Set_70cm/150um/2/build/CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -41,7 +41,7 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
-  "/usr/local/GATE/geant4.10.06.p01-install/include/Geant4"
+  "/usr/local/HEP/GATE/geant4.10.06.p01-install/include/Geant4"
   "/usr/include/qt4/QtGui"
   "/usr/include/qt4/QtCore"
   "/usr/share/qt4/mkspecs/default"
