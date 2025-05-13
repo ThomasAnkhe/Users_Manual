ea9df9a898b24e52537b7856386bf6467d166f41
Rufuskty <t.andrade@uniandes.edu.co>
2025-05-13 13:18:07 -0500

    Implementation of SNR & CNR

---
+++ General_Source/CMakeFiles/pepi.dir/link.d
@@ -0,0 +1,535 @@
+pepi: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o \
+  CMakeFiles/pepi.dir/pepi.cc.o \
+  CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o \
+  CMakeFiles/pepi.dir/src/PepiController.cc.o \
+  CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o \
+  CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o \
+  CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o \
+  CMakeFiles/pepi.dir/src/PepiEventAction.cc.o \
+  CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o \
+  CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o \
+  CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o \
+  CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o \
+  CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o \
+  CMakeFiles/pepi.dir/src/PepiRun.cc.o \
+  CMakeFiles/pepi.dir/src/PepiRunAction.cc.o \
+  CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o \
+  /home/rufuskty/myGeant4/install/lib/libG4Tree.so \
+  /home/rufuskty/myGeant4/install/lib/libG4FR.so \
+  /home/rufuskty/myGeant4/install/lib/libG4GMocren.so \
+  /home/rufuskty/myGeant4/install/lib/libG4visHepRep.so \
+  /home/rufuskty/myGeant4/install/lib/libG4RayTracer.so \
+  /home/rufuskty/myGeant4/install/lib/libG4VRML.so \
+  /home/rufuskty/myGeant4/install/lib/libG4ToolsSG.so \
+  /home/rufuskty/myGeant4/install/lib/libG4OpenGL.so \
+  /home/rufuskty/myGeant4/install/lib/libG4vis_management.so \
+  /home/rufuskty/myGeant4/install/lib/libG4modeling.so \
+  /home/rufuskty/myGeant4/install/lib/libG4interfaces.so \
+  /home/rufuskty/myGeant4/install/lib/libG4mctruth.so \
+  /home/rufuskty/myGeant4/install/lib/libG4geomtext.so \
+  /home/rufuskty/myGeant4/install/lib/libG4gdml.so \
+  /home/rufuskty/myGeant4/install/lib/libG4error_propagation.so \
+  /home/rufuskty/myGeant4/install/lib/libG4readout.so \
+  /home/rufuskty/myGeant4/install/lib/libG4physicslists.so \
+  /home/rufuskty/myGeant4/install/lib/libG4run.so \
+  /home/rufuskty/myGeant4/install/lib/libG4event.so \
+  /home/rufuskty/myGeant4/install/lib/libG4tracking.so \
+  /home/rufuskty/myGeant4/install/lib/libG4parmodels.so \
+  /home/rufuskty/myGeant4/install/lib/libG4processes.so \
+  /home/rufuskty/myGeant4/install/lib/libG4digits_hits.so \
+  /home/rufuskty/myGeant4/install/lib/libG4track.so \
+  /home/rufuskty/myGeant4/install/lib/libG4particles.so \
+  /home/rufuskty/myGeant4/install/lib/libG4geometry.so \
+  /home/rufuskty/myGeant4/install/lib/libG4materials.so \
+  /home/rufuskty/myGeant4/install/lib/libG4graphics_reps.so \
+  /usr/lib/libQt5Widgets.so.5.15.16 \
+  /usr/lib/libQt5Gui.so.5.15.16 \
+  /usr/lib/libQt5Core.so.5.15.16 \
+  /usr/lib/libxerces-c.so \
+  /home/rufuskty/myGeant4/install/lib/libG4analysis.so \
+  /usr/lib/libexpat.so \
+  /home/rufuskty/myGeant4/install/lib/libG4zlib.so \
+  /home/rufuskty/myGeant4/install/lib/libG4intercoms.so \
+  /home/rufuskty/myGeant4/install/lib/libG4global.so \
+  /home/rufuskty/myGeant4/install/lib/libG4clhep.so \
+  /home/rufuskty/myGeant4/install/lib/libG4ptl.so.3.0.0 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o \
+  /usr/lib/libICE.so.6 \
+  /usr/lib/libSM.so.6 \
+  /usr/lib32/libX11.so.6 \
+  /usr/lib/libX11.so.6 \
+  /usr/lib32/libXext.so.6 \
+  /usr/lib/libXext.so.6 \
+  /usr/lib/libXmu.so.6 \
+  /usr/lib/libXt.so.6 \
+  /usr/lib32/libGL.so.1 \
+  /usr/lib/libGL.so.1 \
+  /usr/lib/libQt5OpenGL.so.5 \
+  /usr/lib32/libpng16.so.16 \
+  /usr/lib/libpng16.so.16 \
+  /usr/lib32/libz.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib32/libharfbuzz.so.0 \
+  /usr/lib/libharfbuzz.so.0 \
+  /usr/lib/libmd4c.so.0 \
+  /usr/lib32/libsystemd.so.0 \
+  /usr/lib/libsystemd.so.0 \
+  /usr/lib/libdouble-conversion.so.3 \
+  /usr/lib32/libicui18n.so.76 \
+  /usr/lib/libicui18n.so.76 \
+  /usr/lib32/libicuuc.so.76 \
+  /usr/lib/libicuuc.so.76 \
+  /usr/lib32/libpcre2-16.so.0 \
+  /usr/lib/libpcre2-16.so.0 \
+  /usr/lib32/libzstd.so.1 \
+  /usr/lib/libzstd.so.1 \
+  /usr/lib32/libglib-2.0.so.0 \
+  /usr/lib/libglib-2.0.so.0 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libnsl.so.3 \
+  /usr/lib/libnsl.so.3 \
+  /usr/lib32/libcurl.so.4 \
+  /usr/lib/libcurl.so.4 \
+  /usr/lib32/libicudata.so.76 \
+  /usr/lib/libicudata.so.76 \
+  /usr/lib32/libuuid.so.1 \
+  /usr/lib/libuuid.so.1 \
+  /usr/lib32/libxcb.so.1 \
+  /usr/lib/libxcb.so.1 \
+  /usr/lib32/libGLdispatch.so.0 \
+  /usr/lib/libGLdispatch.so.0 \
+  /usr/lib32/libGLX.so.0 \
+  /usr/lib/libGLX.so.0 \
+  /usr/lib32/libfreetype.so.6 \
+  /usr/lib/libfreetype.so.6 \
+  /usr/lib/libgraphite2.so.3 \
+  /usr/lib32/libcap.so.2 \
+  /usr/lib/libcap.so.2 \
+  /usr/lib32/libpcre2-8.so.0 \
+  /usr/lib/libpcre2-8.so.0 \
+  /usr/lib32/libtirpc.so.3 \
+  /usr/lib/libtirpc.so.3 \
+  /usr/lib32/libnghttp3.so.9 \
+  /usr/lib/libnghttp3.so.9 \
+  /usr/lib32/libnghttp2.so.14 \
+  /usr/lib/libnghttp2.so.14 \
+  /usr/lib32/libidn2.so.0 \
+  /usr/lib/libidn2.so.0 \
+  /usr/lib32/libssh2.so.1 \
+  /usr/lib/libssh2.so.1 \
+  /usr/lib32/libpsl.so.5 \
+  /usr/lib/libpsl.so.5 \
+  /usr/lib32/libssl.so.3 \
+  /usr/lib/libssl.so.3 \
+  /usr/lib32/libcrypto.so.3 \
+  /usr/lib/libcrypto.so.3 \
+  /usr/lib32/libgssapi_krb5.so.2 \
+  /usr/lib/libgssapi_krb5.so.2 \
+  /usr/lib32/libbrotlidec.so.1 \
+  /usr/lib/libbrotlidec.so.1 \
+  /usr/lib32/libXau.so.6 \
+  /usr/lib/libXau.so.6 \
+  /usr/lib32/libXdmcp.so.6 \
+  /usr/lib/libXdmcp.so.6 \
+  /usr/lib32/libbz2.so.1.0 \
+  /usr/lib/libbz2.so.1.0 \
+  /usr/lib32/libunistring.so.5 \
+  /usr/lib/libunistring.so.5 \
+  /usr/lib32/libkrb5.so.3 \
+  /usr/lib/libkrb5.so.3 \
+  /usr/lib32/libk5crypto.so.3 \
+  /usr/lib/libk5crypto.so.3 \
+  /usr/lib32/libcom_err.so.2 \
+  /usr/lib/libcom_err.so.2 \
+  /usr/lib32/libkrb5support.so.0 \
+  /usr/lib/libkrb5support.so.0 \
+  /usr/lib32/libkeyutils.so.1 \
+  /usr/lib/libkeyutils.so.1 \
+  /usr/lib32/libresolv.so.2 \
+  /usr/lib/libresolv.so.2 \
+  /usr/lib32/libbrotlicommon.so.1 \
+  /usr/lib/libbrotlicommon.so.1
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o:
+
+CMakeFiles/pepi.dir/pepi.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiController.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiEventAction.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiRun.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiRunAction.cc.o:
+
+CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o:
+
+/home/rufuskty/myGeant4/install/lib/libG4Tree.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4FR.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4GMocren.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4visHepRep.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4RayTracer.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4VRML.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4ToolsSG.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4OpenGL.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4vis_management.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4modeling.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4interfaces.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4mctruth.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4geomtext.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4gdml.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4error_propagation.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4readout.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4physicslists.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4run.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4event.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4tracking.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4parmodels.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4processes.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4digits_hits.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4track.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4particles.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4geometry.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4materials.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4graphics_reps.so:
+
+/usr/lib/libQt5Widgets.so.5.15.16:
+
+/usr/lib/libQt5Gui.so.5.15.16:
+
+/usr/lib/libQt5Core.so.5.15.16:
+
+/usr/lib/libxerces-c.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4analysis.so:
+
+/usr/lib/libexpat.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4zlib.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4intercoms.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4global.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4clhep.so:
+
+/home/rufuskty/myGeant4/install/lib/libG4ptl.so.3.0.0:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o:
+
+/usr/lib/libICE.so.6:
+
+/usr/lib/libSM.so.6:
+
+/usr/lib32/libX11.so.6:
+
+/usr/lib/libX11.so.6:
+
+/usr/lib32/libXext.so.6:
+
+/usr/lib/libXext.so.6:
+
+/usr/lib/libXmu.so.6:
+
+/usr/lib/libXt.so.6:
+
+/usr/lib32/libGL.so.1:
+
+/usr/lib/libGL.so.1:
+
+/usr/lib/libQt5OpenGL.so.5:
+
+/usr/lib32/libpng16.so.16:
+
+/usr/lib/libpng16.so.16:
+
+/usr/lib32/libz.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib32/libharfbuzz.so.0:
+
+/usr/lib/libharfbuzz.so.0:
+
+/usr/lib/libmd4c.so.0:
+
+/usr/lib32/libsystemd.so.0:
+
+/usr/lib/libsystemd.so.0:
+
+/usr/lib/libdouble-conversion.so.3:
+
+/usr/lib32/libicui18n.so.76:
+
+/usr/lib/libicui18n.so.76:
+
+/usr/lib32/libicuuc.so.76:
+
+/usr/lib/libicuuc.so.76:
+
+/usr/lib32/libpcre2-16.so.0:
+
+/usr/lib/libpcre2-16.so.0:
+
+/usr/lib32/libzstd.so.1:
+
+/usr/lib/libzstd.so.1:
+
+/usr/lib32/libglib-2.0.so.0:
+
+/usr/lib/libglib-2.0.so.0:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libnsl.so.3:
+
+/usr/lib/libnsl.so.3:
+
+/usr/lib32/libcurl.so.4:
+
+/usr/lib/libcurl.so.4:
+
+/usr/lib32/libicudata.so.76:
+
+/usr/lib/libicudata.so.76:
+
+/usr/lib32/libuuid.so.1:
+
+/usr/lib/libuuid.so.1:
+
+/usr/lib32/libxcb.so.1:
+
+/usr/lib/libxcb.so.1:
+
+/usr/lib32/libGLdispatch.so.0:
+
+/usr/lib/libGLdispatch.so.0:
+
+/usr/lib32/libGLX.so.0:
+
+/usr/lib/libGLX.so.0:
+
+/usr/lib32/libfreetype.so.6:
+
+/usr/lib/libfreetype.so.6:
+
+/usr/lib/libgraphite2.so.3:
+
+/usr/lib32/libcap.so.2:
+
+/usr/lib/libcap.so.2:
+
+/usr/lib32/libpcre2-8.so.0:
+
+/usr/lib/libpcre2-8.so.0:
+
+/usr/lib32/libtirpc.so.3:
+
+/usr/lib/libtirpc.so.3:
+
+/usr/lib32/libnghttp3.so.9:
+
+/usr/lib/libnghttp3.so.9:
+
+/usr/lib32/libnghttp2.so.14:
+
+/usr/lib/libnghttp2.so.14:
+
+/usr/lib32/libidn2.so.0:
+
+/usr/lib/libidn2.so.0:
+
+/usr/lib32/libssh2.so.1:
+
+/usr/lib/libssh2.so.1:
+
+/usr/lib32/libpsl.so.5:
+
+/usr/lib/libpsl.so.5:
+
+/usr/lib32/libssl.so.3:
+
+/usr/lib/libssl.so.3:
+
+/usr/lib32/libcrypto.so.3:
+
+/usr/lib/libcrypto.so.3:
+
+/usr/lib32/libgssapi_krb5.so.2:
+
+/usr/lib/libgssapi_krb5.so.2:
+
+/usr/lib32/libbrotlidec.so.1:
+
+/usr/lib/libbrotlidec.so.1:
+
+/usr/lib32/libXau.so.6:
+
+/usr/lib/libXau.so.6:
+
+/usr/lib32/libXdmcp.so.6:
+
+/usr/lib/libXdmcp.so.6:
+
+/usr/lib32/libbz2.so.1.0:
+
+/usr/lib/libbz2.so.1.0:
+
+/usr/lib32/libunistring.so.5:
+
+/usr/lib/libunistring.so.5:
+
+/usr/lib32/libkrb5.so.3:
+
+/usr/lib/libkrb5.so.3:
+
+/usr/lib32/libk5crypto.so.3:
+
+/usr/lib/libk5crypto.so.3:
+
+/usr/lib32/libcom_err.so.2:
+
+/usr/lib/libcom_err.so.2:
+
+/usr/lib32/libkrb5support.so.0:
+
+/usr/lib/libkrb5support.so.0:
+
+/usr/lib32/libkeyutils.so.1:
+
+/usr/lib/libkeyutils.so.1:
+
+/usr/lib32/libresolv.so.2:
+
+/usr/lib/libresolv.so.2:
+
+/usr/lib32/libbrotlicommon.so.1:
+
+/usr/lib/libbrotlicommon.so.1:
--- Simulations/10um/1/build/CMakeFiles/pepi.dir/DependInfo.cmake
+++ Simulations/10um/1/build/CMakeFiles/pepi.dir/DependInfo.cmake
@@ -1,56 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/t.andrade/Legacy/10um/1/pepi.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/pepi.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiActionInitialization.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiController.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiController.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiControllerMessenger.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiDetectorConstruction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiDetectorMessenger.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiEventAction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiEventAction.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiPSPixiRad.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiPhysicsList.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiPhysicsListMessenger.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiPhysicsXrayRefraction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiPrimaryGeneratorAction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiRun.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiRun.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiRunAction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiRunAction.cc.o"
-  "/home/t.andrade/Legacy/10um/1/src/PepiXrayRefraction.cc" "/home/t.andrade/Legacy/10um/1/build/CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "G4INTY_USE_QT"
-  "G4INTY_USE_XT"
-  "G4LIB_BUILD_DLL"
-  "G4UI_USE_QT"
-  "G4UI_USE_TCSH"
-  "G4VIS_USE_OPENGL"
-  "G4VIS_USE_OPENGLQT"
-  "G4VIS_USE_OPENGLX"
-  "QT_CORE_LIB"
-  "QT_GUI_LIB"
-  "QT_NO_DEBUG"
-  "QT_OPENGL_LIB"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
-  "/usr/local/HEP/GATE/geant4.10.06.p01-install/include/Geant4"
-  "/usr/include/qt4/QtGui"
-  "/usr/include/qt4/QtCore"
-  "/usr/share/qt4/mkspecs/default"
-  "/usr/include/qt4"
-  "/usr/include/qt4/QtOpenGL"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/pepi.cc" "CMakeFiles/pepi.dir/pepi.cc.o" "gcc" "CMakeFiles/pepi.dir/pepi.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiActionInitialization.cc" "CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiController.cc" "CMakeFiles/pepi.dir/src/PepiController.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiController.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiControllerMessenger.cc" "CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiDetectorConstruction.cc" "CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiDetectorMessenger.cc" "CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiEventAction.cc" "CMakeFiles/pepi.dir/src/PepiEventAction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiEventAction.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiPSPixiRad.cc" "CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiPhysicsList.cc" "CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiPhysicsListMessenger.cc" "CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiPhysicsXrayRefraction.cc" "CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiPrimaryGeneratorAction.cc" "CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiRun.cc" "CMakeFiles/pepi.dir/src/PepiRun.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiRun.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiRunAction.cc" "CMakeFiles/pepi.dir/src/PepiRunAction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiRunAction.cc.o.d"
+  "/home/rufuskty/repositories/Users_Manual/Simulations/10um/1/src/PepiXrayRefraction.cc" "CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o" "gcc" "CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o.d"
+  "" "pepi" "gcc" "CMakeFiles/pepi.dir/link.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
