fb38718ac36bab8ae9814a123a935cb61047cc30
Rufuskty <t.andrade@uniandes.edu.co>
2025-05-21 16:28:57 -0500

    Little things

---
+++ Simulations/Speckle/Speckel/Speckel_low/build/CMakeFiles/pepi.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/i.avilar/Original_Plaque/Original_Plaque/pepi.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/pepi.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiActionInitialization.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiController.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiController.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiControllerMessenger.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiDetectorConstruction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiDetectorMessenger.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiEventAction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiEventAction.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiPSPixiRad.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiPhysicsList.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiPhysicsListMessenger.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiPhysicsXrayRefraction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiPrimaryGeneratorAction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiRun.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiRun.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiRunAction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiRunAction.cc.o"
+  "/home/i.avilar/Original_Plaque/Original_Plaque/src/PepiXrayRefraction.cc" "/home/i.avilar/Original_Plaque/Original_Plaque/build/CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/HEP/GATE/geant4.10.06.p01-install/include/Geant4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/share/qt4/mkspecs/default"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
